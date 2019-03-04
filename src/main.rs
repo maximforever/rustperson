f6a1e22772ffd1215aaa52681836e1b32a82ed16
Terry O'Shea <terrykoshea@gmail.com>
2019-03-04 17:06:59 -0500

    fix validation of inuputs

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,115 @@
-use std::io::stdin;
+use std::io::{stdin, Read};
 
-use rand::thread_rng;
 use rand::seq::SliceRandom;
+use rand::thread_rng;
 
-const WORD_BANK: [&str; 5] = ["enumerable", "rust", "collection", "borrowing", "iterator" ];
+const WORD_BANK: [&str; 5] = ["enumerable", "rust", "collection", "borrowing", "iterator"];
 const MAX_GUESSES: u8 = 10;
 
-
 struct Game {
-	answer: String,
-	letters_guessed: Vec<char>
+    answer: String,
+    letters_guessed: Vec<char>,
 }
 
+// TODO: think about upper/lower cases
+// TODO: limit the number of guesses
+// TODO: validate input--if characters are not in the Latin alphabet
+// TODO: flush stdout after each turn
+// TODO: bigger words/from different source (API)
+// TODO: multiplayer
+// TODO: show gallows
+// TODO: write tests
+// TODO: code review
+// TODO: display previously guessed letters
+// TODO: do you want to play again?
+
 impl Game {
-	fn setup_new_game(&mut self) {
-		// the vec! can be used to initialize strings
-	    let mut rng = thread_rng();
-	    self.answer = WORD_BANK.choose(&mut rng).unwrap().to_string();	
-	    self.letters_guessed = vec![]; 
+    fn new() -> Game {
+        let mut rng = thread_rng();
+        Game {
+            answer: WORD_BANK.choose(&mut rng).unwrap().to_string(),
+            letters_guessed: vec![],
+        }
+    }
 
-	    println!("Vector: {:?}", WORD_BANK);
-    	println!("Answer: {}", self.answer);
-	}
+    fn setup_new_game(&mut self) {
+        // the vec! can be used to initialize strings
+        let mut rng = thread_rng();
+        self.answer = WORD_BANK.choose(&mut rng).unwrap().to_string();
+        self.letters_guessed = vec![];
 
+        println!("Vector: {:?}", WORD_BANK);
+        println!("Answer: {}", self.answer);
+    }
 
-	fn display_gameboard(&self) -> String {
-		let mut display_answer = String::new();
-		for c in self.answer.chars() {
-			if self.letters_guessed.contains(&c) {
-				display_answer.push(c);
-			} else {
-				display_answer.push('_');
-			}
-		}
-		display_answer			// implicit return
-	}
+    fn display_gameboard(&self) -> String {
+        let mut display_answer = String::new();
+        for c in self.answer.chars() {
+            if self.letters_guessed.contains(&c) {
+                display_answer.push(c);
+            } else {
+                display_answer.push('_');
+            }
+        }
+        display_answer // implicit return
+    }
 
+    fn word_has_been_guessed(&self) -> bool {
+        for c in self.answer.chars() {
+            if !self.letters_guessed.contains(&c) {
+                return false;
+            }
+        }
+        true // implicit return
+    }
 
-	fn word_has_been_guessed(&self) -> bool {
-		for c in self.answer.chars() {
-			if !self.letters_guessed.contains(&c) {
-				return false;
-			}
-		}
-		true			// implicit return
-	}
+    fn is_valid(&self, c: &char) -> bool {
+        c.is_alphabetic()
+    }
 
+    fn get_character(&self) -> char {
+        let mut buffer = String::new();
+        stdin().read_line(&mut buffer).unwrap();
+        let mut c = buffer.to_lowercase().to_string().chars().next().unwrap();
 
-	fn take_turn(&mut self) {
-		let gameboard = self.display_gameboard();
-		println!("Gameboard: {}", gameboard);
+        if self.is_valid(&c) {
+            c
+        } else {
+            self.retry_get_character()
+        }
+    }
 
-		println!("Please guess your next letter.");
-		let mut s = String::new();
-		stdin().read_line(&mut s); // read in user input string
-		s = s.trim().to_string();
+    fn retry_get_character(&self) -> char {
+        println!("Please enter a letter a-z.");
+        self.get_character()
+    }
 
-	    if s.len() != 1 {
-	    	println!("Please only enter one alphabetical character.");
-	    	self.take_turn();
-	    } else {
-	    	let c = s.pop().unwrap();
-	    	// TODO: if characters are not in the Latin alphabet
-	    	if !c.is_alphabetic() {
-	    		println!("Please enter an alphabetical character.");
-	    		self.take_turn();
-	    	} else {
-	    		self.letters_guessed.push(c);
-	    		if self.word_has_been_guessed() {
-	    			println!("You won!");
-	    		} else {
+    fn end_game(&mut self) {
+        println!("Do you want to play again? y/n");
+        let c = self.get_character();
+        if c == 'y' {
+            self.setup_new_game();
+        } else {
+            return;
+        }
+    }
 
-		            self.take_turn();
-	    		}
-	    	}
-	    }
-	}
+    fn take_turn(&mut self) {
+        let gameboard = self.display_gameboard();
+        println!("Gameboard: {}", gameboard);
 
+        println!("Please guess your next letter.");
+        let c = self.get_character();
+        self.letters_guessed.push(c);
+        if self.word_has_been_guessed() {
+            println!("You won!");
+        } else {
+            self.take_turn();
+        }
+    }
 }
 
 fn main() {
-    let mut g = Game {
-    	answer: String::from(""),
-		letters_guessed: vec![]
-    };
-
-    g.setup_new_game();
+    let mut g = Game::new();
     g.take_turn();
 }
-
-
