c09877069f11d15cf5326fefea70cce207da2358
Terry O'Shea <terrykoshea@gmail.com>
2019-03-08 17:57:58 -0500

    Major code improvements
    - add more words to our dictionary
    - refactor methods to make more Rusty (with Austin's help)
    - use BTreeSet instead of vec for letters_guessed
    - reduce code duplication between factory method and game resetting
    - only accept 10 invalid letter entries
    - etc.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
+use std::collections::BTreeSet;
 use std::io::stdin;
 
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use termion::{clear, cursor};
 
-const WORD_BANK: [&str; 5] = ["enumerable", "rust", "collection", "borrowing", "iterator"];
+const WORD_BANK: [&str; 28] = [
+    "attribute",
+    "borrowing",
+    "cargo",
+    "closure",
+    "collection",
+    "compiler",
+    "concurrency",
+    "crate",
+    "crustacean",
+    "enumerable",
+    "functional",
+    "idiomatic",
+    "immutable",
+    "iterator",
+    "lifetime",
+    "macro",
+    "module",
+    "multithreaded",
+    "nightly",
+    "oxide",
+    "panic",
+    "polymorphism",
+    "reference",
+    "rust",
+    "stack",
+    "static",
+    "trait",
+    "unsafe",
+];
 const MAX_GUESSES: usize = 5;
 
 struct Game {
     answer: String,
-    letters_guessed: Vec<char>,
+    letters_guessed: BTreeSet<char>,
     turns_left: usize,
 }
 
 // TODO: write tests
-// TODO: bigger words/from different source (API)
 // TODO: code review
 
 impl Game {
@@ -22,38 +51,26 @@ impl Game {
         let mut rng = thread_rng();
         Game {
             answer: WORD_BANK.choose(&mut rng).unwrap().to_string(),
-            letters_guessed: vec![],
+            letters_guessed: BTreeSet::new(),
             turns_left: MAX_GUESSES,
         }
     }
 
-    fn setup_new_game(&mut self) {
-        // the vec! can be used to initialize strings
-        let mut rng = thread_rng();
-        self.answer = WORD_BANK.choose(&mut rng).unwrap().to_string();
-        self.letters_guessed = vec![];
-        self.turns_left = MAX_GUESSES;
-    }
-
     fn display_gameboard(&self) -> String {
-        let mut display_answer = String::new();
-        for c in self.answer.chars() {
-            if self.letters_guessed.contains(&c) {
-                display_answer.push(c);
-            } else {
-                display_answer.push('_');
-            }
-        }
-        display_answer // implicit return
+        self.answer
+            .chars()
+            .map(|c| match self.letters_guessed.contains(&c) {
+                true => c,
+                false => '_',
+            })
+            .collect()
     }
 
     fn word_has_been_guessed(&self) -> bool {
-        for c in self.answer.chars() {
-            if !self.letters_guessed.contains(&c) {
-                return false;
-            }
-        }
-        true // implicit return
+        self.answer
+            .chars()
+            .collect::<BTreeSet<char>>()
+            .is_subset(&self.letters_guessed)
     }
 
     fn is_valid(&self, c: &char) -> bool {
@@ -67,36 +84,40 @@ impl Game {
     fn get_character(&self) -> char {
         let mut buffer = String::new();
         stdin().read_line(&mut buffer).unwrap();
-        let c = buffer.to_lowercase().chars().next().unwrap();
-
-        // TODO: prevent stack overflow (`yes | cargo run`)
-        // Limit number of guesses to 10
-        if self.is_valid(&c) {
-            c
-        } else {
-            self.retry_get_character()
-        }
+        buffer.to_lowercase().chars().next().unwrap()
     }
 
-    fn retry_get_character(&self) -> char {
-        println!("Please enter a letter a-z.");
-        self.get_character()
+    fn get_character_until_valid_or_timeout(&mut self) -> Option<char> {
+        let mut guess_attempts = 0;
+
+        while guess_attempts < 10 {
+            let c = self.get_character();
+            if self.is_valid(&c) {
+                return Some(c);
+            } else {
+                println!("Please enter a letter a-z.");
+                guess_attempts += 1;
+            }
+        }
+
+        println!("Oh no! Your invalid guesses broke our game :(");
+        None
     }
 
     fn end_game(&mut self) {
         println!("Do you want to play again? y/n");
-        let c = self.get_character();
-        if c == 'y' {
-            self.setup_new_game();
-            self.take_turn();
-        } else {
-            return;
+        match self.get_character() {
+            'y' => {
+                run();
+            }
+            _ => println!("Okay, bye!"),
         }
     }
 
     fn take_turn(&mut self) {
         if self.turns_left == 0 {
             println!("You ran out of guesses :(");
+            println!("Your word was {}.", self.answer.to_uppercase());
             self.end_game();
         } else {
             let gameboard = self.display_gameboard();
@@ -109,31 +130,36 @@ impl Game {
             );
             println!("Guesses left: {}", self.turns_left);
             println!("You've already guessed: {:?}", self.letters_guessed);
-            println!("Please guess your next letter.");
+            println!("Please guess a letter.");
 
-            let c = self.get_character();
+            if let Some(c) = self.get_character_until_valid_or_timeout() {
+                if !self.letters_guessed.contains(&c) {
+                    self.letters_guessed.insert(c);
+                }
 
-            if !self.letters_guessed.contains(&c) {
-                self.letters_guessed.push(c);
-            }
+                if !self.answer.contains(&c.to_string()) {
+                    self.turns_left -= 1;
+                }
 
-            if !self.answer.contains(&c.to_string()) {
-                self.turns_left -= 1;
-            }
-
-            if self.word_has_been_guessed() {
-                println!("You won! 🎉");
-                self.end_game();
-            } else {
-                self.take_turn();
+                if self.word_has_been_guessed() {
+                    println!("Your word was {}.", self.answer.to_uppercase());
+                    println!("You won! 🎉");
+                    self.end_game();
+                } else {
+                    self.take_turn();
+                }
             }
         }
     }
 }
 
-fn main() {
+fn run() {
     let mut g = Game::new();
     g.take_turn();
+}
+
+fn main() {
+    run();
 }
 
 #[cfg(test)]
